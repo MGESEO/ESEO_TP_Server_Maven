97bc5b085724ca439aec8dea17517284bcbe81b0
Mohamed <mohamed.guety@reseau.eseo.fr>
2020-11-10 16:49:33 +0100

    Update Ville class

--- src/main/java/com/dto/Ville.java
+++ src/main/java/com/dto/Ville.java
@@ -1,54 +1,56 @@
 package com.dto;
 
 public class Ville {
-	public String codeCommune;
 	
-	public String nomCommune;
-
-	public String codePostal;
-
-	public String libelleAcheminement;
-
-	public String ligne;
-
+	private String codeCommune;
+	private String nomCommune;
+	private String codePostal;
+	private String libelleAcheminement;	
+	private String ligne;
+	private String longitude;
+	private String latitude;
+	
 	public String getCodeCommune() {
 		return codeCommune;
 	}
-
 	public void setCodeCommune(String codeCommune) {
 		this.codeCommune = codeCommune;
 	}
-
 	public String getNomCommune() {
 		return nomCommune;
 	}
-
 	public void setNomCommune(String nomCommune) {
 		this.nomCommune = nomCommune;
 	}
-
 	public String getCodePostal() {
 		return codePostal;
 	}
-
 	public void setCodePostal(String codePostal) {
 		this.codePostal = codePostal;
 	}
-
 	public String getLibelleAcheminement() {
 		return libelleAcheminement;
 	}
-
 	public void setLibelleAcheminement(String libelleAcheminement) {
 		this.libelleAcheminement = libelleAcheminement;
 	}
-
 	public String getLigne() {
 		return ligne;
 	}
-
 	public void setLigne(String ligne) {
 		this.ligne = ligne;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
 	}
 
 }
